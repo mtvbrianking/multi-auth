8e6f457e08a8d974895ba55d76e8d84834949acf
Brian Matovu <mtvbrianking@gmail.com>
2018-06-20 16:42:28 +0300

    Add php unit tests

---
+++ tests/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bmatovu\MultiAuth\Test;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Bmatovu\MultiAuth\Test;
+
+use Bmatovu\MultiAuth\MultiAuthServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+abstract class TestCase extends Orchestra
+{
+
+    /**
+     * Setup test environment.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Add package service provider
+     *
+     * @param $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            MultiAuthServiceProvider::class
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+}
