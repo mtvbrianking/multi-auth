ea3cae8098af7bf47325123f85f94a58b7ff4b30
mtvbrianking <mtvbrianking@gmail.com>
2019-03-29 16:15:35 +0300

    Fix orchestra testbench errors

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,7 @@ abstract class TestCase extends Orchestra
     /**
      * Setup test environment.
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
